09fb4aa92c4dc46d253b458cc49c78589c460b21
nobrien97 <nicholasobrien97@gmail.com>
2020-07-21 15:59:22 +1000

    Model Aims Document and Time Test
    -Document describing how my models achieve my aims, theorising what resources I will need for production runs
    - Files for last time test, for the 2nd aim.

---
+++ src/R/8T100L_recom_timetest_worstcase.R
@@ -0,0 +1,4 @@
+
+# Worst case scenario test run for 8T Selection
+
+system.time(system("/home/$USER/SLiM/slim -s 123 -d rregion=0.5 -d Ne=10000 -d QTL_cov=0.5 -d pleiorate=1.0 -d delmu=0.0 -d rwide=1.346e-5 /home/$USER/SLiM/Scripts/null_recom_8T100L_nowrite.slim"))
