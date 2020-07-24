67c2941c464bc605419a2d84bb4aecb67759bc82
nobrien97 <nicholasobrien97@gmail.com>
2020-07-24 17:51:21 +1000

    More heterozygosity/burn-in analysis

--- src/R/heterozygosity_test.R
+++ src/R/heterozygosity_test.R
@@ -27,38 +27,51 @@ system.time(system("wsl slim -s 123 heterozygosity_burnin_test.slim",
                            ))
 
 library(ggplot2)
+library(gghighlight)
+
+# Simple function to calculate standard error
+
+std.error <-  function(x) {
+  n <- length(x)
+  sd <- sd(x)
+  sd/sqrt(n)
+}
 
 dat_het <-read.csv("heterozygosity.csv", header = F)
 
-names(dat_het) <- c("gens", "seed", "mean", "var")
-#dat_het$gens = seq(500, (length(dat_het$mean) * 500), 500)
+names(dat_het) <- c("gens", "seed", "Ne", "rwide", "delmu", "mean", "var")
+dat_het$delmu <- factor(dat_het$delmu, labels = c("No~deleterious~mutations", "Equal~chance~deleterious/neutral")) 
+dat_het$rwide <- factor(dat_het$rwide, labels = c("r~'='~0", "r~'='~1.346~'x'~10^{-5}")) 
+dat_het$seed <- as.factor(dat_het$seed)
 
+
+# Calculate group means and standard errors to plot
+
+library(dplyr)
+
+dat_het_means <- dat_het[c(1, 3:6)] %>%
+                   group_by(Ne, delmu, rwide, gens) %>%
+                   summarise_all(list(groupmean = mean, se = std.error))
+  
 
 library(coda)
 
-mcmc_het <- mcmc(dat_het$mean)
-autocorr.plot(mcmc_het, lag.max = 50)
+mcmc_het <- mcmc(dat_het$mean[dat_het$Ne == 10000 & dat_het$rwide > 0.0 & dat_het$delmu == 0.0])
+autocorr.plot(mcmc_het, lag.max = 100)
 autocorr.plot(dat_het$mean, lag.max = 50)
 
-EPibounds <- as.data.frame(c(0.0252+(0.0252*0.05), 0.0252-(0.0252*0.05)))
-names(EPibounds) <- "pibounds"
 
-plot_het_mean <- ggplot(dat_het,
-                        aes(x = gens, y = mean, color = as.factor(seed))) +
-  geom_line() +
-  geom_hline(
-    data = EPibounds,
-    aes(yintercept = pibounds), linetype="dashed"
-  ) +
-  ggtitle("Mean heterozygosity across QTL loci over time") +
-  theme_classic() +
-  theme(legend.position = "none") +
-  labs(x = "Generation", y = "Mean heterozygosity")
+seedchoice <- sample(unique(dat_het$seed), 2)
+
+
+ 
+ plot_het <- ggplot(dat_het_means,
+                         aes(x = gens, y = groupmean, color = as.factor(Ne))) +
+    geom_ribbon(aes(ymin = (groupmean - se), ymax = (groupmean + se)), alpha=0.3, show.legend = F, linetype=0) +
+    geom_line() +
+    facet_grid(delmu ~ ., scales = "free", labeller = label_parsed) +
+    ggtitle("Mean heterozygosity across QTL loci over time") +
+#    theme_classic() +
+#    theme(legend.position = "right") +
+    labs(x = "Generation", y = "Mean heterozygosity", color = "Population size")
   
-plot_het_var <- ggplot(dat_het,
-                        aes(x = gens, y = var, color = as.factor(seed))) +
-  geom_line() +
-  ggtitle("Variance in heterozygosity across QTL loci among individuals over time") +
-  theme_classic() +
-  theme(legend.position = "none") +
-  labs(x = "Generation", y = "Heterozygosity variance")
