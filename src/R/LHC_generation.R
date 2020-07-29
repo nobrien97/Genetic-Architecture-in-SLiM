fc527af85c866bd8789792d299030c62edf00b69
nobrien97 <nicholasobrien97@gmail.com>
2020-07-29 16:38:08 +1000

    Minor changes to stabsel, LHC setup
    - LHC setup: changes to parameter ranges, justifications
    - Fixed bug in stabsel which occasionally crashed the run, added parameter for adjusting selection strength

--- src/R/LHC_generation.R
+++ src/R/LHC_generation.R
@@ -20,6 +20,8 @@
 
 # Genome-wide recombination rate standard deviation (rsd): Ranges from Standard deviation used in the rnorm() 
 # function to get recombination rates for chromosomes
+# May fix this at 0.1 * rwide: can't go much higher on the max recombination, it'll get too slow, and recombination rates 
+# vary too much to effectively sample with everything else to do in a timely manner
 
 # Number of loci (nloci): Ranges from 10 to 500: very few loci affecting a trait to many 
 # (from experimental results from QTL studies - McKown et al. 2014; Ehrenreich et al. 2010; Lu et al. 2010)
@@ -34,14 +36,16 @@
 # characters. Also, Dudley et al. 2005 found 58% reduced growth in two or more conditions
 
 # Rate of deleterious mutation (delmu): Ranges from 0.0 to 1.0: 
-# No deleterious mutations to equal chance to a neutral mutation
+# No deleterious mutations to equal chance to a neutral mutation - as expected with neutral theory
 
 # Pleiotropic mutational correlation (pleio_cov) - this defines the magnitude and direction of 
 # correlations between traits in pleiotropic mutations, used in the multivariate normal pull to get 
 # effects for all traits: Ranges from -0.5 to 0.5: 
 # Ranges from strong negative to strong positive mutational correlations, 0 represents pure pleiotropy
 
-# Number of chromosomes to have deleterious mutations: Ranges from 0 to 10, from none of them to all of them
+# Number of chromosomes to have deleterious mutations (delchr): Ranges from 0 to 10, from none of them to all of them
+
+# Strength of selection (s): multiplier for fitness function: ranges from 1 to 100 (weak to very strong selection)
 
 
 # Generate latin hypercube sample for all the factors
@@ -49,17 +53,18 @@
 library(DoE.wrapper)
 
 lscombos <- lhs.design(
-  nruns = 100, # 100 is too few, but this drastically increases the amount of runs: nruns * nseeds is the total. Also generating the LHC takes forever with more of these
+  nruns = 100, # this drastically increases the amount of runs: nruns * nseeds is the total. Also generating the LHC takes forever with more of these
   nfactors = 9,
   seed = 1370976025, #sampled from 1:2147483647
   #  type = "improved",
   factor.names = list(
-    rregions = c(0.0, 0.5),
-    rwide = c(0.0, 1.346e-5),  # Try it, don't know how long it will run!
     Ne = c(10, 10000),
-    pleiocov = c(-0.5, 0.5), # More even sampling, going from -0.5 to 0.5 rather than 0 to 0.5
+    rwide = c(0.0, 1.907e-3), 
+    nloci = c(10, 500),
+    locisigma = c(0.1, 10),
     pleiorate = c(0.0, 0.5),
-    delmu = c(0.0, 1.0))
-  #    npercent = c(0.00001, 0.8)) # Based on genome size of 100000 - Sinnott-Armstrong et al. 2020 estimate ~80,000 sites affecting human height, so we sample from 1 to 80000 QTLs
-  
+    delmu = c(0.0, 1.0),
+    pleiocov = c(-0.5, 0.5), # More even sampling, going from -0.5 to 0.5 rather than 0 to 0.5
+    delchr = c(0, 10),
+    s = c(1, 100))
 )
