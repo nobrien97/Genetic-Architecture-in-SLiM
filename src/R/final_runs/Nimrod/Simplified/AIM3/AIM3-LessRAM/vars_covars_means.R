4ee058f40b475d299040d98adddf5e18853208ef
nobrien97 <nicholasobrien97@gmail.com>
2020-10-15 17:13:41 +1000

    covariance plots

--- src/R/final_runs/Nimrod/Simplified/AIM3/AIM3-LessRAM/vars_covars_means.R
+++ src/R/final_runs/Nimrod/Simplified/AIM3/AIM3-LessRAM/vars_covars_means.R
@@ -177,3 +177,129 @@ grid.newpage()
 grid.draw(plot_gtab_var.d.r.ls)
 
 
+
+############################################################################################################
+############################################################################################################
+############################################################################################################
+
+# Plot covariances
+
+# Delmu * rwide * ls
+dplot_cov_d.r.ls <- d_raw_mat[,c(7, 12, 13, 52)] %>%
+  group_by(delmu.cat, rwide.cat, locisigma.cat) %>%
+  summarise_all(list(cov_mean = mean, cov_se = std.error))
+
+plot_cov_r.d.ls <- ggplot(dplot_cov_d.r.ls, aes(x = rwide.cat, y = cov_mean, fill = delmu.cat)) +
+  facet_grid(locisigma.cat~.) +
+  geom_col(position = position_dodge(0.9)) +
+  geom_errorbar(aes(
+    ymin = cov_mean - (1.96*cov_se), 
+    ymax = cov_mean + (1.96*cov_se)), 
+    width = 0.25,
+    position = position_dodge(0.9)) +
+  scale_fill_npg() +
+  theme_classic() +
+  labs(x = r_lab, y = "Mean coviance across traits", fill = d_lab) +
+  theme(strip.text.x = element_text(size = 12),
+        strip.text.y = element_text(size = 12),
+        axis.text.x = element_text(size = 10, face = "bold"),
+        axis.title.y = element_text(margin = margin(r = 10), face = "bold"),
+        axis.title.x = element_text(margin = margin(t = 10), face = "bold"),
+        text = element_text(size = 12))
+
+# Thanks to: https://stackoverflow.com/a/37292665/13586824
+# Add delmu label
+# Labels 
+library(grid)
+library(gtable)
+labelR = ls_lab
+
+# Get the ggplot grob
+plot_gtab <- ggplotGrob(plot_cov_r.d.ls)
+
+# Get the positions of the strips in the gtable: t = top, l = left, ...
+posR <- subset(plot_gtab$layout, grepl("strip-r", name), select = t:r)
+
+# Add a new column to the right of current right strips, 
+# and a new row on top of current top strips
+width <- plot_gtab$widths[max(posR$r)]    # width of current right strips
+
+plot_gtab <- gtable_add_cols(plot_gtab, width, max(posR$r))  
+
+# Construct the new strip grobs
+stripR <- gTree(name = "Strip_right", children = gList(
+  rectGrob(gp = gpar(col = NA, lwd = 3.0, col = "black")),
+  textGrob(labelR, rot = -90, gp = gpar(fontsize = 12, col = "black", fontface = "bold"))))
+
+# Position the grobs in the gtable
+plot_gtab <- gtable_add_grob(plot_gtab, stripR, t = min(posR$t), l = max(posR$r) + 1, b = max(posR$b), name = "strip-right")
+
+# Add small gaps between strips
+plot_gtab_cov.r.d.ls <- gtable_add_cols(plot_gtab, unit(1/5, "line"), max(posR$r))
+
+# Draw it
+grid.newpage()
+grid.draw(plot_gtab_cov.r.d.ls)
+
+
+############################################################################################################
+############################################################################################################
+############################################################################################################
+# Delmu * rwide * ls
+
+plot_cov_d.r.ls <- ggplot(dplot_cov_d.r.ls, aes(x = delmu.cat, y = cov_mean, fill = rwide.cat)) +
+  facet_grid(locisigma.cat~.) +
+  geom_col(position = position_dodge(0.9)) +
+  geom_errorbar(aes(
+    ymin = cov_mean - (1.96*cov_se), 
+    ymax = cov_mean + (1.96*cov_se)), 
+    width = 0.25,
+    position = position_dodge(0.9)) +
+  scale_fill_npg() +
+  theme_classic() +
+  labs(x = d_lab, y = "Mean coviance across traits", fill = r_lab) +
+  theme(strip.text.x = element_text(size = 12),
+        strip.text.y = element_text(size = 12),
+        axis.text.x = element_text(size = 10, face = "bold"),
+        axis.title.y = element_text(margin = margin(r = 10), face = "bold"),
+        axis.title.x = element_text(margin = margin(t = 10), face = "bold"),
+        text = element_text(size = 12))
+
+# Thanks to: https://stackoverflow.com/a/37292665/13586824
+# Add delmu label
+# Labels 
+library(grid)
+library(gtable)
+labelR = ls_lab
+
+# Get the ggplot grob
+plot_gtab <- ggplotGrob(plot_cov_d.r.ls)
+
+# Get the positions of the strips in the gtable: t = top, l = left, ...
+posR <- subset(plot_gtab$layout, grepl("strip-r", name), select = t:r)
+
+# Add a new column to the right of current right strips, 
+# and a new row on top of current top strips
+width <- plot_gtab$widths[max(posR$r)]    # width of current right strips
+
+plot_gtab <- gtable_add_cols(plot_gtab, width, max(posR$r))  
+
+# Construct the new strip grobs
+stripR <- gTree(name = "Strip_right", children = gList(
+  rectGrob(gp = gpar(col = NA, lwd = 3.0, col = "black")),
+  textGrob(labelR, rot = -90, gp = gpar(fontsize = 12, col = "black", fontface = "bold"))))
+
+# Position the grobs in the gtable
+plot_gtab <- gtable_add_grob(plot_gtab, stripR, t = min(posR$t), l = max(posR$r) + 1, b = max(posR$b), name = "strip-right")
+
+# Add small gaps between strips
+plot_gtab_cov.d.r.ls <- gtable_add_cols(plot_gtab, unit(1/5, "line"), max(posR$r))
+
+# Draw it
+grid.newpage()
+grid.draw(plot_gtab_cov.d.r.ls)
+
+
+
+
+
