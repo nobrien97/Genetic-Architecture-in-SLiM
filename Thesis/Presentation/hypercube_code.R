c34bb05cc19d3315369e272320330daa22d21954
nobrien97 <nicholasobrien97@gmail.com>
2020-11-20 15:00:32 +1000

    Update hypercube_code.R

--- Thesis/Presentation/hypercube_code.R
+++ Thesis/Presentation/hypercube_code.R
@@ -1,4 +1,21 @@
 library(Cairo)
+library(plot3D)
+setwd("Z:/Documents/GitHub/Genetic-Architecture-in-SLiM/src/Cluster_jobs/final_runs/Nimrod/Simplified/AIM1")
+
+lscombos_null <- read.csv("Z:/Documents/GitHub/Genetic-Architecture-in-SLiM/src/Cluster_jobs/final_runs/Nimrod/Simplified/AIM1/lscombos_null.csv")
+lscombos_sel <- read.csv("Z:/Documents/GitHub/Genetic-Architecture-in-SLiM/src/Cluster_jobs/final_runs/Nimrod/Simplified/AIM3/lscombos_sel.csv")
+
+lscombos_sel$X <- lscombos_sel$X + 1024
+lscombos_null$tau <- 0.0
+
+lscombos <- data.table::rbindlist(list(lscombos_null, lscombos_sel))
+
 CairoWin()
-LHCscatter <- scatter3D(lscombos$rwide, lscombos$locisigma, lscombos$delmu, colvar = NULL, ticktype = "detailed", xlab = "Recombination rate ", ylab = "Additive effect size variance", zlab = "QTL mutation rate", phi = 2)
+
+size <- par(cex = 1)
+LHCscatter <- scatter3D(lscombos$rwide, lscombos$locisigma, lscombos$delmu, colvar = NULL, xlab = "", 
+                        ylab = "", zlab = "", phi = 2
+                        )
+
 CairoPNG("LHCscatter.png", LHCscatter)
+
