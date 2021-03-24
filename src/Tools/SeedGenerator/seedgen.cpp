f490e6c30efac92968a7af46cf4e57e381a9d796
nobrien97 <nicholasobrien97@gmail.com>
2021-03-24 15:46:52 +1000

    - Updated number generator to use /dev/random and Mersenne Twister generation for more (pseudo)randomness

--- src/Tools/SeedGenerator/seedgen.cpp
+++ src/Tools/SeedGenerator/seedgen.cpp
@@ -3,13 +3,21 @@
 #include <random>
 #include <string>
 #include <vector>
+#include <getopt.h>
+#include <chrono>
 
 using std::endl;
-using std::cin;
 using std::cout;
 
-void write_csv(std::string filename, std::vector<int32_t> values) {
+#define no_argument 0
+#define required_argument 1
+
+void write_csv(std::string filename, std::vector<int32_t> values, std::string header = "Seed") {
     std::ofstream outfile(filename);
+
+    if (header.size()) {
+        outfile << header << "\n";
+    }
     
     for (int i = 0; i < values.size(); ++i) {
     if (i < values.size() - 1) {
@@ -22,15 +30,74 @@ void write_csv(std::string filename, std::vector<int32_t> values) {
     outfile.close();
 }
 
-int main() {
-    std::default_random_engine generator;
-    std::uniform_int_distribution<int32_t> distribution(1, INT32_MAX - 1);
-    int n_samples;
+
+void doHelp(char* appname) {
+    std::fprintf(stdout,
+    "Uniformly Distributed Seed Generator\n"
+    "\n"
+    "This program generates a .csv of uniformly distributed 32-bit integers for use as RNG seeds.\n"
+    "Usage: %s [OPTION]...\n"
+    "Example: %s -h\n"
+    "\n"
+    "-h     Print this help manual.\n"
+    "\n"
+    "-n N   Generate N random samples.\n"
+    "\n"
+    "-d FILEPATH    Specify a filepath and name for the generated seeds to be saved.\n"
+    "Example: -d ~/Desktop/seeds.csv\n"
+    "\n",
+    appname,
+    appname
+    );
+
+}
+
+int main(int argc, char* argv[]) {
+    
+
+    int optionindex = 0;
+
+    const struct option longopts[] =
+    {
+        { "destination",    no_argument,        0,  'd' },
+        { "nsamples",       required_argument,  0,  'n' },
+        { "help",           no_argument,        0,  'h' },
+        {0,0,0,0}
+    };
+
     std::string filename;
-    cout << "Seeds to generate: " << endl;
-    cin >> n_samples;
-    cout << "Filepath and name: " << endl;
-    cin >> filename;
+    int n_samples;
+
+   int options; 
+
+    while (options != -1) {
+
+        options = getopt_long(argc, argv, "n:d:h", longopts, &optionindex);
+
+        switch (options) {
+            case 'n':
+                n_samples = std::stoi(optarg);
+                continue;
+
+            case 'd':
+                filename = optarg;
+                continue;
+
+            case 'h':
+                doHelp(argv[0]);
+                return 1;
+
+            case -1:
+                break;
+            }
+        }
+
+
+    std::random_device mersseed;
+    cout << mersseed() << endl;
+
+    std::mt19937 generator(mersseed());
+    std::uniform_int_distribution<int32_t> distribution(1, INT32_MAX - 1);
 
     std::vector<int32_t> seeds;
 
