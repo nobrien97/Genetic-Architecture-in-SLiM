ca0f11df8530e1bdab7be18891812d7ccc5e4df2
nobrien97 <nicholasobrien97@gmail.com>
2021-03-31 18:09:20 +1000

    Analysis of model function - Figures of fixations and seg mutations with different levels of genetic constraint - Plot function library to quickly draw rough ggplots depending on input

---
+++ Paper/src/R/includes/plot_function.R
@@ -0,0 +1,125 @@
+# Collection of useful functions for visualising data using ggplot, wrapped within an interface function plot_maker
+
+
+library(tidyverse)
+
+# Excessively ugly functions with lots of repeated code, epic
+
+plot_maker <- function(dat, type = c("d", "l"), x, y, xlab, ylab, group, colour, group.lab, colour.lab, leg.enabled, savename) {
+  if (missing(type))
+    return("Please choose a figure type")
+  else {
+    # Create a list/enum struct of plot types to offload to the correct plot function
+    type <- match.arg(type)
+    argv <- as.list(match.call(expand.dots = TRUE)[-1]) # argv minus the function name
+    
+    # Then check if we have a filename and use that to select which arguments to keep for the plot method
+    if (!missing(savename)) {
+      argv_feed <- argv[-match(c(type, savename), argv)]
+    }
+    
+    else
+      argv_feed <- argv[-match(type, argv)]
+    
+      
+    switch (type,
+      d = do.call(density_plot, argv_feed), # Get rid of the type argument
+      l = do.call(line_plot, argv_feed)
+    )
+      
+  }
+  if (!missing(savename)) {
+    ggsave(savename, last_plot(), width = 15, height = 15)
+  }
+  else last_plot() # BAD
+    
+    
+}
+
+# Plot a density plot
+
+density_plot <- function(dat, x, xlab="x", group, colour, group.lab = group, colour.lab = colour, leg.enabled=TRUE) {
+  
+  if (missing(group) & missing(colour)) {
+   gg_temp <- ggplot(data = dat, 
+           aes_string(x = x)) +
+      geom_density() +
+      labs(x = xlab, y = "Density")
+
+  }
+  else if (!missing(group) & missing(colour)) {
+    gg_temp <- ggplot(data = dat, 
+           aes_string(x = x, group = group)) +
+      geom_density() +
+      labs(x = xlab, y = "Density", group = group.lab)
+
+  }
+  else { # if we have a specified colour, use that for grouping. If we have both grouping and colour, do colour only
+    gg_temp <- ggplot(data = dat, 
+           aes_string(x = x, colour = colour)) +
+      geom_density() +
+      labs(x = xlab, y = "Density", colour = colour.lab)
+
+  }
+  gg_temp <- gg_temp +  
+    theme_classic() +
+    theme(axis.text.x = element_text(size = 16, margin = margin(t = 8), face = "bold"),
+                         axis.title.y = element_text(margin = margin(r = 10), face = "bold", family = "Lucida Sans Unicode"),
+                         axis.title.x = element_text(size = 22, margin = margin(t = 10), face = "bold"),
+                         plot.title = element_text(margin = margin(t = 20), face = "bold", hjust = 0.5),
+                         text = element_text(size = 22),
+                         legend.key.width = unit(1.6, "cm"),
+                         legend.title.align = 0.5,
+                         panel.spacing.y = unit(1, "lines"))
+  
+  if (leg.enabled == FALSE)
+    gg_temp <- gg_temp + theme(legend.position = "none")
+  
+  
+  gg_temp
+}
+
+
+
+# Plot a line graph
+
+line_plot <- function(dat, x, y, xlab="x", ylab="y", group, colour, group.lab = group, colour.lab = colour, leg.enabled = TRUE) {
+  if (missing(group) & missing(colour)) {
+    gg_temp <- ggplot(data = dat, 
+           aes_string(x = x, y = y)) +
+      geom_line() +
+      labs(x = xlab, y = ylab)
+  }
+  else if (!missing(group) & missing(colour)) {
+    gg_temp <- ggplot(data = dat, 
+           aes_string(x = x, y = y, group = group)) +
+      geom_line() +
+      labs(x = xlab, y = ylab, group = group.lab) 
+  }
+  else { # if we have a specified colour, use that for grouping. If we have both grouping and colour, do colour only
+    gg_temp <- ggplot(data = dat, 
+           aes_string(x = x, y = y, colour = colour)) +
+      geom_line() +
+      labs(x = xlab, y = ylab, colour = colour.lab)
+
+  }
+  gg_temp <- gg_temp + 
+    theme_classic() +
+    theme(axis.text.x = element_text(size = 16, margin = margin(t = 8), face = "bold"),
+                             axis.title.y = element_text(margin = margin(r = 10), face = "bold", family = "Lucida Sans Unicode"),
+                             axis.title.x = element_text(size = 22, margin = margin(t = 10), face = "bold"),
+                             plot.title = element_text(margin = margin(t = 20), face = "bold", hjust = 0.5),
+                             text = element_text(size = 22),
+                             legend.key.width = unit(1.6, "cm"),
+                             legend.title.align = 0.5,
+                             panel.spacing.y = unit(1, "lines"))
+  
+  if (leg.enabled == FALSE)
+    gg_temp <- gg_temp + theme(legend.position = "none")
+  
+  gg_temp
+  
+}
+
+
+# Plot an 
--- Paper/src/R/tests/GeneticConstraint/geneticconstraint_dists.R
+++ Paper/src/R/tests/GeneticConstraint/geneticconstraint_dists.R
@@ -1,6 +1,8 @@
 
 # Import data
 
+setwd("/mnt/z/Documents/GitHub/Genetic-Architecture-in-SLiM/Paper/data/tests/GeneticConstraint/noLD/")
+
 d_burnmeans <- read.csv("out_stabsel_burnin.csv", header = F)
 
 names(d_burnmeans) <- c("gen", "seed", "modelindex", "meanH", "VA", "phenomean")
@@ -9,9 +11,14 @@ d_means <- read.csv("out_stabsel_means.csv", header = F)
 
 names(d_means) <- c("gen", "seed", "modelindex", "meanH", "VA", "phenomean", "dist", "mean_w")
 
-d_muts <- read.csv("out_stabsel_muts.csv", header = F)[,1:10] # Ignore extra empty column (fixed in next version of script)
+d_muts <- read.csv("out_stabsel_muts.csv", header = F) # Ignore extra empty column (fixed in next version of script)
 
-names(d_muts) <- c("gen", "seed", "modelindex", "mutType", "mutID", "position", "originGen", "value", "Freq", "fixGen")
+names(d_muts) <- c("gen", "seed", "modelindex", "mutType", "mutID", "position", "constraint", "originGen", "value", "Freq", "fixGen")
+
+d_muts$constraint <- as.factor(d_muts$constraint)
+
+levels(d_muts$constraint) <- c("Low", "Medium", "High")
+
 
 d_pos <- read.csv("out_stabsel_pos.csv", header = F)
 
@@ -49,14 +56,222 @@ for (seed in unique(d_dict_long$seed)) {
 }
 
 library(tidyverse)
-
+library(colorspace)
 
 # In this example, d_dict and d_pos show that this seed has QTLs at pos 0, 2, 3; and those are constrained according to 
 # medium, low, high constraint respectively
 
-ggplot(d_muts[d_muts$fixGen != "N/A",], 
-       aes(value)) +
-  facet_grid(~constraints) +
-  geom_freqpoly() +
+plot_fixations <- ggplot(d_muts[d_muts$fixGen != "N/A" & d_muts$mutType == 3 & d_muts$gen == 150000,], 
+                          aes(x = value, colour = constraint)) +
+                          scale_color_discrete_sequential() +
+                          geom_density() +
+                          theme_classic() +
+                          ggtitle("Distribution of effect sizes among fixations") +
+                          labs(x = "Trait Value", y = "Density", colour = "Genetic Constraint") +
+  theme(axis.text.x = element_text(size = 16, margin = margin(t = 8), face = "bold"),
+        axis.title.y = element_text(margin = margin(r = 10), face = "bold", family = "Lucida Sans Unicode"),
+        axis.title.x = element_text(size = 22, margin = margin(t = 10), face = "bold"),
+        plot.title = element_text(margin = margin(t = 20), face = "bold", hjust = 0.5),
+        text = element_text(size = 22),
+        legend.key.width = unit(1.6, "cm"),
+        legend.title.align = 0.5,
+        panel.spacing.y = unit(1, "lines"))
+
+plot_fixations
+
+ggsave("plot_fixations.png", plot = plot_fixations, height = 15, width = 15)
+
+
+plot_seg <- ggplot(d_muts[d_muts$fixGen == "N/A" & d_muts$mutType == 3 & d_muts$gen == 150000,], 
+              aes(x = value, colour = constraint)) + 
+              scale_color_discrete_sequential() +
+              geom_density() +
+              theme_classic() +
+              ggtitle("Distribution of effect sizes among segregating mutations") +
+              labs(x = "Trait Value", y = "Density", colour = "Genetic Constraint") +
+  theme(axis.text.x = element_text(size = 16, margin = margin(t = 8), face = "bold"),
+        axis.title.y = element_text(margin = margin(r = 10), face = "bold", family = "Lucida Sans Unicode"),
+        axis.title.x = element_text(size = 22, margin = margin(t = 10), face = "bold"),
+        plot.title = element_text(margin = margin(t = 20), face = "bold", hjust = 0.5),
+        text = element_text(size = 22),
+        legend.key.width = unit(1.6, "cm"),
+        legend.title.align = 0.5,
+        panel.spacing.y = unit(1, "lines"))
+
+plot_seg
+ggsave("plot_seg.png", plot = plot_seg, height = 15, width = 15)
+
+
+
+
+plot_del_fixations <- ggplot(d_muts[d_muts$fixGen != "N/A" & d_muts$mutType == 2,], 
+                            aes(x = value, colour = constraint)) +
+                            scale_color_discrete_sequential() +
+                            geom_density() +
+                            theme_classic() +
+                            ggtitle("Distribution of deleterious fitness effects among fixations") +
+                            labs(x = "Selection coefficient", y = "Density", colour = "Genetic Constraint") +
+  theme(axis.text.x = element_text(size = 16, margin = margin(t = 8), face = "bold"),
+        axis.title.y = element_text(margin = margin(r = 10), face = "bold", family = "Lucida Sans Unicode"),
+        axis.title.x = element_text(size = 22, margin = margin(t = 10), face = "bold"),
+        plot.title = element_text(margin = margin(t = 20), face = "bold", hjust = 0.5),
+        text = element_text(size = 22),
+        legend.key.width = unit(1.6, "cm"),
+        legend.title.align = 0.5,
+        panel.spacing.y = unit(1, "lines"))
+
+
+plot_del_fixations
+ggsave("plot_del_fixations.png", plot = plot_del_fixations, height = 15, width = 15)
+
+
+
+plot_del_seg <- ggplot(d_muts[d_muts$fixGen == "N/A" & d_muts$mutType == 2,], 
+                   aes(x = value, colour = constraint)) + 
+  scale_color_discrete_sequential() +
+  geom_density() +
   theme_classic() +
-  labs(x = "Trait Value", y = "Density")
+  ggtitle("Distribution of deleterious fitness effects among segregating mutations") +
+  labs(x = "Selection coefficient", y = "Density", colour = "Genetic Constraint") +
+  theme(axis.text.x = element_text(size = 16, margin = margin(t = 8), face = "bold"),
+        axis.title.y = element_text(margin = margin(r = 10), face = "bold", family = "Lucida Sans Unicode"),
+        axis.title.x = element_text(size = 22, margin = margin(t = 10), face = "bold"),
+        plot.title = element_text(margin = margin(t = 20), face = "bold", hjust = 0.5),
+        text = element_text(size = 22),
+        legend.key.width = unit(1.6, "cm"),
+        legend.title.align = 0.5,
+        panel.spacing.y = unit(1, "lines"))
+
+plot_del_seg
+ggsave("plot_del_seg.png", plot = plot_del_seg, height = 15, width = 15)
+
+
+
+plot_phenoburn <- ggplot(d_burnmeans, 
+                         aes(x = gen, y = phenomean, group = seed)) +
+  scale_color_discrete_sequential() +
+  geom_line() +
+  theme_classic() +
+  ggtitle("Drift of phenotypic means over time") +
+  labs(x = "Time (generations)", y = "Phenotypic mean") +
+  theme(axis.text.x = element_text(size = 16, margin = margin(t = 8), face = "bold"),
+        axis.title.y = element_text(margin = margin(r = 10), face = "bold", family = "Lucida Sans Unicode"),
+        axis.title.x = element_text(size = 22, margin = margin(t = 10), face = "bold"),
+        plot.title = element_text(margin = margin(t = 20), face = "bold", hjust = 0.5),
+        text = element_text(size = 22),
+        legend.key.width = unit(1.6, "cm"),
+        legend.title.align = 0.5,
+        panel.spacing.y = unit(1, "lines"))
+
+plot_phenoburn
+
+
+plot_pheno <- ggplot(d_means, 
+                         aes(x = gen, y = phenomean, group = seed)) +
+  scale_color_discrete_sequential() +
+  geom_line() +
+  theme_classic() +
+  ggtitle("Movement of phenotypic means over time with stabilising selection") +
+  labs(x = "Time (generations)", y = "Phenotypic mean") +
+  theme(axis.text.x = element_text(size = 16, margin = margin(t = 8), face = "bold"),
+        axis.title.y = element_text(margin = margin(r = 10), face = "bold", family = "Lucida Sans Unicode"),
+        axis.title.x = element_text(size = 22, margin = margin(t = 10), face = "bold"),
+        plot.title = element_text(margin = margin(t = 20), face = "bold", hjust = 0.5),
+        text = element_text(size = 22),
+        legend.key.width = unit(1.6, "cm"),
+        legend.title.align = 0.5,
+        panel.spacing.y = unit(1, "lines"))
+
+plot_pheno
+
+
+
+# Extreme values: Here, high constraint means neutral mutations are 50x and deleterious 100x more likely than QTL mutations
+# Medium constraint: neutral are 5x more likely and deleterious 10x more likely than QTL
+# low constraint: each type are equally likely
+
+setwd("/mnt/z/Documents/GitHub/Genetic-Architecture-in-SLiM/Paper/data/tests/GeneticConstraint/extremeconstraints/")
+
+d_burnmeans_ex <- read.csv("out_stabsel_burnin.csv", header = F)
+
+names(d_burnmeans_ex) <- c("gen", "seed", "modelindex", "meanH", "VA", "phenomean")
+
+d_means_ex <- read.csv("out_stabsel_means.csv", header = F)
+
+names(d_means_ex) <- c("gen", "seed", "modelindex", "meanH", "VA", "phenomean", "dist", "mean_w")
+
+d_muts_ex <- read.csv("out_stabsel_muts.csv", header = F) # Ignore extra empty column (fixed in next version of script)
+
+names(d_muts_ex) <- c("gen", "seed", "modelindex", "mutType", "mutID", "position", "constraint", "originGen", "value", "Freq", "fixGen")
+
+d_muts_ex$constraint <- as.factor(d_muts_ex$constraint)
+
+levels(d_muts_ex$constraint) <- c("Low", "Medium", "High")
+
+d_muts_ex$Tfix <- NA
+
+d_muts_ex[d_muts_ex$fixGen != "N/A",]$Tfix <- as.integer(d_muts_ex[d_muts_ex$fixGen != "N/A",]$fixGen) - d_muts_ex[d_muts_ex$fixGen != "N/A",]$originGen  # Time to fixation
+
+
+plot_ex_fixations <- ggplot(d_muts_ex[d_muts_ex$fixGen != "N/A" & d_muts_ex$mutType == 3 & d_muts_ex$gen == 150000,], 
+                         aes(x = value, colour = constraint)) +
+  scale_color_discrete_sequential() +
+  geom_density() +
+  theme_classic() +
+  ggtitle("Distribution of effect sizes among fixations") +
+  labs(x = "Trait Value", y = "Density", colour = "Genetic Constraint") +
+  theme(axis.text.x = element_text(size = 16, margin = margin(t = 8), face = "bold"),
+        axis.title.y = element_text(margin = margin(r = 10), face = "bold", family = "Lucida Sans Unicode"),
+        axis.title.x = element_text(size = 22, margin = margin(t = 10), face = "bold"),
+        plot.title = element_text(margin = margin(t = 20), face = "bold", hjust = 0.5),
+        text = element_text(size = 22),
+        legend.key.width = unit(1.6, "cm"),
+        legend.title.align = 0.5,
+        panel.spacing.y = unit(1, "lines"))
+
+plot_ex_fixations
+
+
+plot_ex_seg <- ggplot(d_muts_ex[d_muts_ex$fixGen == "N/A" & d_muts_ex$mutType == 3,], 
+                   aes(x = value, colour = constraint)) + 
+  scale_color_discrete_sequential() +
+  geom_density() +
+  theme_classic() +
+  ggtitle("Distribution of effect sizes among segregating mutations") +
+  labs(x = "Trait Value", y = "Density", colour = "Genetic Constraint") +
+  theme(axis.text.x = element_text(size = 16, margin = margin(t = 8), face = "bold"),
+        axis.title.y = element_text(margin = margin(r = 10), face = "bold", family = "Lucida Sans Unicode"),
+        axis.title.x = element_text(size = 22, margin = margin(t = 10), face = "bold"),
+        plot.title = element_text(margin = margin(t = 20), face = "bold", hjust = 0.5),
+        text = element_text(size = 22),
+        legend.key.width = unit(1.6, "cm"),
+        legend.title.align = 0.5,
+        panel.spacing.y = unit(1, "lines"))
+
+plot_ex_seg
+
+
+
+plot_ex_Tfix <- ggplot(d_muts_ex[!is.na(d_muts_ex$Tfix) & d_muts_ex$mutType == 3,], 
+                       aes(x = Tfix, colour = constraint)) + 
+  scale_color_discrete_sequential() +
+  geom_density() +
+  theme_classic() +
+  ggtitle("Time to fixation among QTL mutations") +
+  labs(x = "Time (generations)", y = "Density", colour = "Genetic Constraint") +
+  theme(axis.text.x = element_text(size = 16, margin = margin(t = 8), face = "bold"),
+        axis.title.y = element_text(margin = margin(r = 10), face = "bold", family = "Lucida Sans Unicode"),
+        axis.title.x = element_text(size = 22, margin = margin(t = 10), face = "bold"),
+        plot.title = element_text(margin = margin(t = 20), face = "bold", hjust = 0.5),
+        text = element_text(size = 22),
+        legend.key.width = unit(1.6, "cm"),
+        legend.title.align = 0.5,
+        panel.spacing.y = unit(1, "lines"))
+
+plot_ex_Tfix
+
+
+source("/mnt/z/Documents/GitHub/Genetic-Architecture-in-SLiM/Paper/src/R/includes/plot_function.R")
+
+plot_maker(d_muts_ex[!is.na(d_muts_ex$Tfix) & d_muts_ex$mutType == 3,], type = "d", x="Tfix", colour="constraint")
+
