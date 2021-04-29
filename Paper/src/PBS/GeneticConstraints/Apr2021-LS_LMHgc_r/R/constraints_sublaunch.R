8a3fd0b575ff458da1678219fe2cc6706e42d205
nobrien97 <nicholasobrien97@gmail.com>
2021-04-29 13:11:27 +1000

    HPC Run for Fixed High, Medium, Low genetic constraint - HPC run to test genome-wide recombination, locisigma, and genome-wide genetic constraint using 48 seeds

---
+++ Paper/src/PBS/GeneticConstraints/Apr2021-LS_LMHgc_r/R/constraints_sublaunch.R
@@ -0,0 +1,50 @@
+##############################################################################################################
+#  Genetic Constraint run: 48 replicates, stabilising selection, 40 genes, 30 QTLs, recom, locisigma, gen con
+##############################################################################################################
+
+
+#  Parallel script modified from SLiM-Extras example R script, info at
+#  the SLiM-Extras repository at https://github.com/MesserLab/SLiM-Extras.
+
+# Environment variables
+
+USER <- Sys.getenv('USER')
+ARR_INDEX <- as.numeric(Sys.getenv('PBS_ARRAY_INDEX'))
+
+
+# Parallelisation libraries 
+
+library(foreach)
+library(doParallel)
+library(future)
+
+
+seeds <- read.csv(paste0("/home/",USER,"/SLiM/Scripts/Tests/GeneticConstraints/Apr2021-LS_LMHgc_r/R/seeds_48.csv"), header = T)
+combos <- read.csv(paste0("/home/",USER,"/SLiM/Scripts/Tests/GeneticConstraints/Apr2021-LS_LMHgc_r/R/combos.csv"), header = T)
+
+# Set which runs to do according to node
+
+switch (object,
+  case = action
+)
+
+switch (ARR_INDEX,
+  { combos <- combos[1:5,] },
+  { combos <- combos[6:9,] },
+  { combos <- combos[10:13,] },
+  { combos <- combos[14:18,] }
+)
+
+
+cl <- makeCluster(future::availableCores())
+registerDoParallel(cl)
+
+#Run SLiM
+foreach(i=combos$modelindex) %:%
+  foreach(j=seeds$Seed) %dopar% {
+	# Use string manipulation functions to configure the command line args, feeding from a data frame of seeds
+	# then run SLiM with system(),
+    	slim_out <- system(sprintf("/home/$USER/SLiM/slim -s %s -d locisigma=%f -d rwide=%f -d con_props=%s -d modelindex=%i /home/$USER/SLiM/Scripts/Tests/GeneticConstraints/Apr2021-LS_LMHgc_r/slim/polygen_maint.slim", as.character(j), combos[i,]$locisigma, combos[i,]$rwide, combos[i,]$gencon, as.integer(i), intern=T))
+  }
+stopCluster(cl)
+
---
+++ Paper/src/PBS/GeneticConstraints/Apr2021-LS_LMHgc_r/readme.txt
@@ -0,0 +1,9 @@
+This test changes levels of locisigma (1 or 10), genetic constraint (genome wide being low, medium, or high), and recombination (0, 0.2, 0.5)
+48 total seeds/replicates
+
+SFS_Test showed 24 replicates taking an hour and 5 minutes 30 seconds for 95000 generations
+Expand out to 150000 generations, get 131/76hrs/ 1.72hrs
+48 replicates should take double that, 3.447hrs (131/38)
+18 total levels to do in this factorial design, with 48 replicates each
+18 * 131/38 = 62.05 hours total if using 1 node
+With 4 nodes, that's 15.5 hours total
