791760dde9b9fb644fd981871e2e735d92411002
nobrien97 <nicholasobrien97@gmail.com>
2021-04-29 15:07:17 +1000

    Updated combos - Proper formatting of gencon so that it can be read by SLiM (enclosed with quotes and as a float vector)

--- Paper/src/PBS/GeneticConstraints/Apr2021-LS_LMHgc_r/R/combos.R
+++ Paper/src/PBS/GeneticConstraints/Apr2021-LS_LMHgc_r/R/combos.R
@@ -1,8 +1,9 @@
 # Generate combinations of variables and save as a csv
 
 #parameters
+p <- list()
 p$locisigma <- c(1.0, 10.0)
-p$gencon <- c("c(1, 0, 0)", "c(0, 1, 0)", "c(0, 0, 1)")
+p$gencon <- c('"c(1.0, 0.0, 0.0)"', '"c(0.0, 1.0, 0.0)"', '"c(0.0, 0.0, 1.0)"')
 p$rwide <- c(0.0, 0.2, 0.5)
 
 write.csv(expand.grid(p), "combos.csv")
