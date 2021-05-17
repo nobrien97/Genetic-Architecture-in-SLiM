f8a587bfd196baf8408627ce9246a23709f61bc8
nobrien97 <nicholasobrien97@gmail.com>
2021-05-17 18:35:17 +1000

    Gadi speed test R sublauncher

---
+++ Paper/src/PBS/GeneticConstraints/May2021_GadiSLiM_speedtest/R/slimtest.R
@@ -0,0 +1,34 @@
+##############################################################################################################
+#  Gadi single core speed test
+##############################################################################################################
+
+
+#  Parallel script modified from SLiM-Extras example R script, info at
+#  the SLiM-Extras repository at https://github.com/MesserLab/SLiM-Extras.
+
+# Environment variables
+
+HOME <- Sys.getenv('HOME')
+
+# Parallelisation libraries 
+
+library(foreach)
+library(doParallel)
+library(future)
+
+
+seeds <- read.csv(paste0(HOME"/Tools/SeedGen/seeds.csv"), header = T)
+
+
+cl <- makeCluster(future::availableCores())
+registerDoParallel(cl)
+
+#Run SLiM
+
+foreach(i=seeds$Seed) %dopar% {
+	# Use string manipulation functions to configure the command line args, feeding from a data frame of seeds
+	# then run SLiM with system(),
+    	slim_out <- system(sprintf("$HOME/SLiM/build/slim -s %s $HOME/SLiM/Tests/GadiSLiM_speedtest/slim/polygen_maint.slim", as.character(i), intern=T))
+  }
+stopCluster(cl)
+
