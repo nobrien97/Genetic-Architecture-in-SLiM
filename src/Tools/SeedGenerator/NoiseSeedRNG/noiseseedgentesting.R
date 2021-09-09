c7d50485c1e3ff7695733d46709dce99cc3a0a16
nobrien97 <nicholasobrien97@gmail.com>
2021-09-09 14:17:27 +1000

    Updated noiseseedgen - fixed perf issue

--- src/Tools/SeedGenerator/NoiseSeedRNG/noiseseedgentesting.R
+++ src/Tools/SeedGenerator/NoiseSeedRNG/noiseseedgentesting.R
@@ -5,3 +5,6 @@ df <- read.csv("seeds.csv")
 # Test uniformity
 hist(df$Seed)
 range(df$Seed)
+
+system.time(system("/mnt/c/GitHub/Genetic-Architecture-in-SLiM/src/Tools/SeedGenerator/NoiseSeedRNG/noiseseedgenerator -l -n 10000000"))
+system.time(system("/mnt/c/GitHub/Genetic-Architecture-in-SLiM/src/Tools/SeedGenerator/Linux/seedgenerator -l -n 10000000"))
