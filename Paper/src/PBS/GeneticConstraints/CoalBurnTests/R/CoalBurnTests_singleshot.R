b9a094c422b7ab961c348edde27ac93311ff9d59
nobrien97 <nicholasobrien97@gmail.com>
2021-11-16 16:49:37 +1000

    adding LD calculation based on MAF

--- Paper/src/PBS/GeneticConstraints/CoalBurnTests/R/CoalBurnTests_singleshot.R
+++ Paper/src/PBS/GeneticConstraints/CoalBurnTests/R/CoalBurnTests_singleshot.R
@@ -27,29 +27,34 @@ HOME <- Sys.getenv("HOME")
 seeds <- read.csv(paste0(HOME, "/tests/CoalBurnTests/R/seeds_coalburntest.csv"), header = T)
 combos <- read.csv(paste0(HOME, "/tests/CoalBurnTests/R/lhc_coalburntest.csv"), header = T)
 
+# Set timer
+time <- system.time(
+  # Check if we are a control or tree model
+  if(combos$tree[i]){
+    # Sublaunch Python to generate the tree sequence (conda sucks)
+    system(sprintf("$HOME/.conda/envs/msslim/bin/python3.9 $HOME/tests/CoalBurnTests/R/burnin.py %i %s",
+                  i,
+                  as.character(seeds$Seed[j]),
+                  intern=T))
 
-# Check if we are a control or tree model
-if ( combos$tree[i] ) {
-  # Sublaunch Python to generate the tree sequence (conda sucks)
-  system(sprintf("$HOME/.conda/envs/msslim/bin/python3.9 $HOME/tests/CoalBurnTests/R/burnin.py %i %s", 
-                i, 
-                as.character(seeds$Seed[j]), 
-                intern = T))
+    # Run SLiM
+    slim_out <- system(sprintf("$HOME/slim/slim -s %s -d Ne=%i -d rwide=%f -d modelindex=%i $HOME/tests/CoalBurnTests/slim/polygen_maint_coalload.slim",
+                                as.character(seeds$Seed[j]),
+                                as.integer(ceiling(combos[i,]$Ne)),
+                                combos[i,]$rwide,
+                                i,intern=T))
+  } else{
+    # Sublaunch SLiM with the appropriate values
+    slim_out <- system(sprintf("$HOME/slim/slim -s %s -d Ne=%i -d rwide=%f -d modelindex=%i $HOME/tests/CoalBurnTests/slim/polygen_maint.slim",
+                            as.character(seeds$Seed[j]),
+                            as.integer(round(combos[i,]$Ne)),
+                            combos[i,]$rwide,
+                            i,intern=T))
 
-  # Run SLiM
-  slim_out <- system(sprintf("$HOME/slim/slim -s %s -d Ne=%i -d rwide=%f -d modelindex=%i $HOME/tests/CoalBurnTests/slim/polygen_maint_coalload.slim", 
-                              as.character(seeds$Seed[j]), 
-                              as.integer(ceiling(combos[i,]$Ne)), 
-                              combos[i,]$rwide, 
-                              i, intern=T))
-} else {
-# Sublaunch SLiM with the appropriate values
-slim_out <- system(sprintf("$HOME/slim/slim -s %s -d Ne=%i -d rwide=%f -d modelindex=%i $HOME/tests/CoalBurnTests/slim/polygen_maint.slim", 
-                           as.character(seeds$Seed[j]), 
-                           as.integer(round(combos[i,]$Ne)), 
-                           combos[i,]$rwide, 
-                           i, intern=T))
+  }
+)[3] # Show elapsed time only
 
-}
-
+# Save the total time taken to run the simulation
+write.table(time, paste0("/scratch/user/uqnobri4/CoalBurnTests/time_", seeds$Seed[j], "_", i), 
+          row.names = F, col.names = F)
 
